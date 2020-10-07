75cbb37aefafae4828c4e1ef917259824efb2ca3
Lena Ryan <alissadyiphil@gmail.com>
2020-10-07 14:43:51 +0300

    adding navbar

---
+++ src/components/Navbar.vue
@@ -0,0 +1,25 @@
+<template>
+  <nav class="deep-purple darken-3">
+    <div class="nav-wrapper container">
+      <ul>
+        <li><a href="/">Books</a></li>
+        <li><a href="/">Add New Book</a></li>
+      </ul>
+    </div>
+  </nav>
+</template>
+
+<script>
+export default {
+  name: 'Navbar',
+  data () {
+    return {
+      
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
